9455deb10b6d032b48dc8d657a988a71ce2f7344
Nathan Scott <nathanscott@saharadigital.co.uk>
2024-07-11 13:32:36 +0100

    removed img requirement and redundant components

--- src/pages/boardgames/index.tsx
+++ src/pages/boardgames/index.tsx
@@ -3,7 +3,6 @@ import { Header, TopHeader } from '../../components/header';
 import { Navigation } from '../../components/navigation';
 import { Footer } from '../../components/footer';
 import { Container, MainContainer } from '../../components/styled';
-import HomeIMG from '../../components/svg/website-images/0_10.png';
 import { BreadCrumb } from '../../components/breadcrumbs';
 
 export const BoardGames = () => {
@@ -31,26 +30,3 @@ const Background = styled('div')`
     left: 0;
     z-index: -2;
 `;
-
-// const Container = styled('div')`
-//     color: #10000E;
-//     height: 625px;
-//     width: 100%;
-//     position: absolute;
-//     top: 0;
-//     left: 0;
-//     z-index: -1;
-//     background-image: linear-gradient(to bottom, black, #05030F);
-// `;
-
-// const ImageContainer = styled('div')`
-//     background-image: url(${HomeIMG});
-//     background-repeat: no-repeat;
-//     background-size: cover;
-//     height: 100%;
-//     width: 100%;
-//     position: absolute;
-//     top: 0;
-//     left: 0;
-//     opacity: 0.5;
-// `;
