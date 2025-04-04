334d3bfc67563af54363c85df979b96a808bb0de
NathanScott85 <nathanscott@saharadigital.co.uk>
2025-04-04 12:55:02 +0100

    set environment variables

--- src/apollo-client/index.ts
+++ src/apollo-client/index.ts
@@ -1,38 +1,36 @@
 import { ApolloClient, InMemoryCache, ApolloLink } from '@apollo/client';
-
 import createUploadLink from 'apollo-upload-client/createUploadLink.mjs';
 
-const isProduction = process.env.NODE_ENV === 'production';
-const apiUri = isProduction
-    ? 'https://your-production-api.com/graphql'
-    : 'http://localhost:4000/graphql';
+const apiUri =
+    process.env.REACT_APP_API_URL || 'http://localhost:4000/graphql';
 
+const isProduction = process.env.NODE_ENV === 'production';
 const credentialsPolicy = isProduction ? 'include' : 'same-origin';
 
-const httpLink = createUploadLink( {
+const httpLink = createUploadLink({
     uri: apiUri,
     credentials: credentialsPolicy,
     headers: {
         'x-apollo-operation-name': 'upload',
         'apollo-require-preflight': 'true',
     },
-} );
+});
 
-const authLink = new ApolloLink( ( operation, forward ) => {
-    const accessToken = sessionStorage.getItem( 'accessToken' );
-    const refreshToken = sessionStorage.getItem( 'refreshToken' );
+const authLink = new ApolloLink((operation, forward) => {
+    const accessToken = sessionStorage.getItem('accessToken');
+    const refreshToken = sessionStorage.getItem('refreshToken');
 
-    operation.setContext( {
+    operation.setContext({
         headers: {
             Authorization: accessToken ? `Bearer ${accessToken}` : '',
             'x-refresh-token': refreshToken || '',
         },
-    } );
+    });
 
-    return forward( operation );
-} );
+    return forward(operation);
+});
 
-export const client = new ApolloClient( {
-    link: authLink.concat( httpLink ),
+export const client = new ApolloClient({
+    link: authLink.concat(httpLink),
     cache: new InMemoryCache(),
-} );
+});
